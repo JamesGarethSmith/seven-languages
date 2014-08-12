83da85ec4532881a33c56663aef90ce06b701dd4
James Gareth Smith <james.gareth.smith@gmail.com>
2014-08-12 18:16:25 +0200

    removed inadvertant conversion of strings into int 0

--- ruby/random_guess.rb
+++ ruby/random_guess.rb
@@ -3,16 +3,17 @@ answer = rand(9) + 1
 puts "Guess a number between 1 and 10"
 
 while true
-  guess = gets.to_i
+  guess = gets
+  integer = Integer(guess) rescue false
 
-  if guess > answer
+  if ! integer
+    puts "Please only enter a whole number from 1 to 10"
+  elsif guess.to_i > answer
     puts "High, guess again"
-  elsif guess < answer
+  elsif guess.to_i < answer
     puts "Low, guess again"
-  elsif guess == answer
+  else
     puts "You got it the answer was #{answer}"
     break
-  else
-    puts "Please only enter a whole number from 1 to 10"
   end
 end
